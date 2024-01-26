b479254f8c1c36064e92cec24fbe6acd92d56a4d
saitejamoroju <104844024+saitejamoroju@users.noreply.github.com>
2024-01-26 17:39:34 +0000

    commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript
+frst integrate node with js 
+go to view & command palatte,search container
+click modify 
+search node & java script then select default ,then click ok
---
+++ index.js
@@ -0,0 +1,13 @@
+const id=12345;
+var userName="saiteja"
+let userMarks=100
+address="Nalgonda"//can be decalred without data type
+console.log(id);
+console.log(userName);
+console.table([id,userName,userMarks,address]);
+
+/*
+Note:
+Avoid using Var keyword because block and functional 
+scope
+*/
