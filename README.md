f1d6423a020f9063025832f8007cacb7314d78cb
saitejamoroju <104844024+saitejamoroju@users.noreply.github.com>
2024-01-27 05:00:58 +0000

    done

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ frst integrate node with js
 go to view & command palatte,search container
 click modify 
 search node & java script then select default ,then click ok
+to run the file
+node datatypes/index.js
--- datatypes/index.js
+++ datatypes/index.js
@@ -1,1 +1,9 @@
 console.log("hello");
+"use strict";//this treats all js code as newer version
+// number -2^53
+// BigInt
+// boolean 
+// null -> (typeof null)=object
+// undefined - this empty one by default-type=undefined
+//symbol-unique
+//typeof-> to check the type 
