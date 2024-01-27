626a3d6c094617ee434b151314f0d804dac5843f
saitejamoroju <104844024+saitejamoroju@users.noreply.github.com>
2024-01-27 09:56:46 +0000

    a

--- datatypes/index.js
+++ datatypes/index.js
@@ -1,9 +1,85 @@
-console.log("hello");
+// console.log("hello");
 "use strict";//this treats all js code as newer version
-// number -2^53
+// Number -2^53
 // BigInt
-// boolean 
-// null -> (typeof null)=object
+// Boolean 
+// Null -> (typeof null)=object
 // undefined - this empty one by default-type=undefined
 //symbol-unique
 //typeof-> to check the type 
+
+
+
+
+//number conversion
+// let score="100";
+// console.log(typeof score);
+// let valueInNumber1=Number(score)//converting into number
+// console.log(typeof valueInNumber1);
+
+// let score2="33abc"
+// let valueInNumber2=Number(score2)//converting into number
+// console.log(valueInNumber2);//op:NaN
+
+// let score1=null
+// let valueInNumber3=Number(score1)//converting into number
+// console.log(valueInNumber3);//op:0
+
+
+// let score1=undefined
+// let valueInNumber3=Number(score1)//converting into number
+// console.log(valueInNumber3);//op:NaN
+
+
+// let score1=undefined
+// let valueInNumber3=String(score1)//converting into number
+// console.log( valueInNumber3);//op:NaN
+
+//*************** operations **************************
+
+//  console.log("1"+2);//12
+//  console.log(1+"2");//12
+//  console.log("1"+2+2);//122
+//  console.log(1+2+"2");//32
+
+// console.log(+true);//1
+// console.log(true+);//error
+
+// console.log("2">1);//true;
+
+// console.log(null>0); //false
+// console.log(null==0);//false
+// console.log(null>=0);//true //coz comparision 
+            //converts null to a number so it treats as 0
+
+
+            //primitive data types
+            //string ,Number,Boolean,null,undefined,symbol,BigInt
+//const bigNumber=(56451511515135156151n)->it can be converted into bigint 
+
+ //NOn primitive data types-Array,objects,functions
+ //arrys
+//  const heros=['a','b','c']
+// //  object
+//     let myObj={
+//         name:"saiteja",
+//         age:22,
+//         address:"Nlg"
+//     }
+// //function
+// const myFunction =function(){
+//     console.log("hello world");
+// }
+// myFunction()
+//---------------------------------------------------
+
+// Stack(primitive ), Heap(Non-Primitive)
+
+// --------------------------------------------------------
+
+// const name="saiteja"
+// const age=21
+// console.log(`My name is ${name} and my age is ${age}`);
+//------------------------------------------------------------
+// const gameName=new String('saiteja')
+// console.log(gameName); //creation of string in the form of object and key value pair
